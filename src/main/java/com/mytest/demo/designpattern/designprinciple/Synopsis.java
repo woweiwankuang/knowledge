7d6edb414085d162b575ac259ea87ca48cc836c9
sikongyan <447524920@qq.com>
2018-08-05 15:34:57 +0800

    状态模式

---
+++ src/main/java/com/mytest/demo/designpattern/designprinciple/Synopsis.java
@@ -0,0 +1,9 @@
+package com.mytest.demo.designpattern.designprinciple;
+
+/**
+ * 一些简单的设计模式的记录
+ */
+public class Synopsis {
+    //1、状态模式
+    //在类中添加一个状态字段（枚举或int），当调用类的某些方法是根据状态执行不同的逻辑，当调用某些方法时改变状态
+}
